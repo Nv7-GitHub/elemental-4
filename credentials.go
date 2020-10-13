98360c6878444787fa045267e3559fcdf2507fd3
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2020-10-12 21:50:34 -0700

    Inteferering with gitignore, deleting

--- credentials.go
+++
@@ -1,7 +0,0 @@
-// This file contains private information, so it is not public. You need a firebase project with Authentication and a Realtime Database. Put the appropriate information below. Contact me by making an issue
-package main
-const (
-	apiKey         string = `API_KEY`
-	databaseURL    string = `DATABASE_URL`
-	serviceAccount string = `SERVICEACCOUNT` // Go to the Project Settings tab, go into the service accounts tab, and press "Generate New Private Key". Then, copy the JSON into this variable
-)
